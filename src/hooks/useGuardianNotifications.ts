8d692721f3f903cb0c73645b4ebb987d37a7dfa7
Priyaranjan <priyaranjan.dev01@gmail.com>
2026-03-25 00:57:51 +0530

    build fix

--- src/hooks/useGuardianNotifications.ts
+++ src/hooks/useGuardianNotifications.ts
@@ -276,7 +276,7 @@ export function useGuardianNotifications(): GuardianState {
       // Subscribe to push
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey) as unknown as BufferSource,
       });
 
       subscriptionRef.current = subscription;
